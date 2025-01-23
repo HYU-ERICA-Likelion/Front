c6e01c4ac82cc8fa5cd75b7ee8ebd0f91b4dc766
ksundev <kshk08@hanyang.ac.kr>
2025-01-24 07:53:37 +0900

    style: 인터뷰 타이틀 컴포넌트 구현 (#9)

---
+++ src/components/interview/title.tsx
@@ -0,0 +1,11 @@
+export default function InterviewTitle() {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="font-bold text-[24px] p-[12px_16px] border-none rounded-[4px] bg-[#F08314]">
+        운영진 소개
+      </div>
+      <div className="h-[12px]" />
+      <div className="font-bold text-[36px]">역대 운영진 인터뷰</div>
+    </div>
+  );
+}
