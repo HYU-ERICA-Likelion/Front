d1f0398a023efa8b788dca1b56f8efe3668eeec2
gn-ioeo <thdrkdms1030@hanyang.ac.kr>
2025-01-24 17:03:38 +0900

    style: 타이틀 구현

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,3 +1,9 @@
 export default function ProjectPage() {
-  return <div>프로젝트 아카이빙 페이지</div>;
+
+  return (
+    <div className="flex flex-col items-center gap-y-[12px] mt-[146px]">
+        <div className="py-[12px] px-[16px] bg-[#F08314] rounded-[4px] text-[24px] font-[700]">프로젝트 아카이빙</div>
+        <div className="text-[36px] font-[700]">멋사 프로젝트 둘러보기</div>
+    </div>
+  );
 }
