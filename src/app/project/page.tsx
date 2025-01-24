47cc38d77d19b0b67a3d13b7de5b9fa257bb4122
gn-ioeo <thdrkdms1030@hanyang.ac.kr>
2025-01-24 18:23:19 +0900

    feat: 기수별 필터 구현

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,9 +1,18 @@
-export default function ProjectPage() {
+"use client";
 
+import ArchivingSelectGen from "@/components/archiving/gen";
+import { useState } from "react";
+
+export default function ProjectPage() {
+  const [gen, setGen] = useState<string>("전체");
   return (
-    <div className="flex flex-col items-center gap-y-[12px] mt-[146px]">
+    <div className="h-screen">
+      <div className="flex flex-col items-center gap-y-[12px] mt-[146px]">
         <div className="py-[12px] px-[16px] bg-[#F08314] rounded-[4px] text-[24px] font-[700]">프로젝트 아카이빙</div>
         <div className="text-[36px] font-[700]">멋사 프로젝트 둘러보기</div>
+      </div>
+      <ArchivingSelectGen selectedGen={gen} setSelectedGen={setGen} />  
     </div>
+    
   );
 }
