ef2cd4d620185f83a7b94a827daba2e5c53f734a
mongsam2 <songjaehyun121476@gmail.com>
2025-01-30 18:20:31 +0900

    style: 엔딩크레딧 애니메이션 정의의

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,6 +23,15 @@ export default {
       fontFamily: {
         pretendard: ["var(--font-pretendard)"],
       },
+      keyframes: {
+        scroll: {
+          "0%": { transform: "translateY(100vh)" },
+          "100%": { transform: "translateY(-100%)" },
+        },
+      },
+      animation: {
+        endingCredit: "scroll 20s linear infinite",
+      },
     },
   },
   plugins: [],
