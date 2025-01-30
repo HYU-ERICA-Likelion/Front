32bb77c162d0fa100b7f6fcc822b3b90f87bcb5e
mongsam2 <songjaehyun121476@gmail.com>
2025-01-30 18:22:52 +0900

    style: 엔딩크레딧 옆에 멋냥이 동영상 추가 (#27)

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,20 +2,22 @@ import AboutListElement from "@/components/About/AboutListElement";
 import AboutList from "@/components/About/AboutList";
 import dataList from "@/constants/about";
 
-
 export default function AboutPage() {
   return (
-    <div className="flex-col justify-between items-center box-border">
+    <div className="flex-col justify-start items-center">
       <h1 className="pretendard text-4xl font-bold text-center p-12 mb-6">
         ~웹 사이트를 만든 사람들~
       </h1>
-      <div className="flex align-start justify-center"> 
+      <div className="flex align-start justify-center overflow-y-hidden mb-12"> 
         {/* content */}
-
-        <div className="w-96 h-96 mr-20 bg-primary"> 
-          <span>동영상 있어요!!!</span>
+        <div className="w-96 h-96 mr-8">
+          <video autoPlay loop muted>
+            <source src="@/../assets/videos/ending-credit.mp4" type="video/mp4" />
+          </video>
         </div>
-        <AboutList dataList={dataList} />
+        <div className="h-96">
+          <AboutList dataList={dataList} />
+        </div>
       </div>
     </div>
   );
