c0a6dd9c170fb099a7a9d65c74f08914c0d7814a
ksundev <kshk08@hanyang.ac.kr>
2025-02-01 06:43:30 +0900

    feat: QnA 컴포넌트 제작 (#41)

--- src/app/interview/[id]/page.tsx
+++ src/app/interview/[id]/page.tsx
@@ -1,4 +1,5 @@
 import Profile from "@/components/interview/profile";
+import QnA from "@/components/interview/QnA";
 
 const img = "/assets/images/jongwon.png";
 
@@ -8,9 +9,12 @@ export default function Interview({
   params: { id: string };
 }) {
   return (
-    <div>
+    <div className="flex flex-col items-center">
       <div className="h-[129px]" />
       <Profile img={img} position="Software Engineer" name="John Doe" />
+      <div className="h-[93px]" />
+      <QnA question={`${id}`} answer={`${id}`} />
+      <div className="h-[200px]" />
     </div>
   );
 }
