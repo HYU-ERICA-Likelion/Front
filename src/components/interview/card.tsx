732852c93bcddbbf2e16fe9fce5e130b758547d9
ksundev <kshk08@hanyang.ac.kr>
2025-02-01 04:37:38 +0900

    feat: 페이지 이동 구현 (#37)

--- src/components/interview/card.tsx
+++ src/components/interview/card.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const img1 = "/assets/images/quotes.svg";
 const img2 = "/assets/images/right-arrow.svg";
 
@@ -6,11 +8,18 @@ interface Props {
   name: string;
   position: string;
   text: string;
+  id: string;
 }
 
-export default function InterviewCard({ name, position, img, text }: Props) {
+export default function InterviewCard({
+  name,
+  position,
+  img,
+  text,
+  id,
+}: Props) {
   return (
-    <div className="flex flex-col bg-[#474343] w-[325px] max-h-[412px] p-[22px_15px_15px_15px] text-center rounded-[14px] items-center">
+    <div className="flex flex-col bg-[#474343] active:bg-[#5E5A5A] w-[325px] max-h-[412px] p-[22px_15px_15px_15px] text-center rounded-[14px] items-center">
       <div className="w-[176px] h-[176px] bg-[#FFFFFF90] overflow-hidden rounded-full flex items-center justify-center border-0">
         <img src={img} className="w-full h-full object-cover" alt="" />
       </div>
@@ -20,13 +29,17 @@ export default function InterviewCard({ name, position, img, text }: Props) {
         <div className="text-[#FFF] text-[20px] font-[500]">{position}</div>
       </div>
       <div className="h-[30px]" />
-      <div className="flex flex-row items-center gap-x-[12px] p-[18.5px_13.5px] bg-[#292725] rounded-[12px]">
+      <Link
+        prefetch
+        href={`/interview/${id}`}
+        className="flex flex-row items-center gap-x-[12px] p-[18.5px_13.5px] bg-[#292725] rounded-[12px]"
+      >
         <img src={img1} alt="quotes" />
         <p className="text-[14px] font-medium w-[200px] break-words line-clamp-2 text-left">
           {text}
         </p>
         <img src={img2} alt="arrow" />
-      </div>
+      </Link>
     </div>
   );
 }
