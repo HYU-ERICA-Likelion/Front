3ded669550f43f5d044ebaf72b305bd732c035f5
mongsam2 <songjaehyun121476@gmail.com>
2025-02-01 19:35:27 +0900

    style: 크기 및 마진 수정 (#7)

--- src/components/About/AboutListElement.tsx
+++ src/components/About/AboutListElement.tsx
@@ -5,13 +5,13 @@ export interface AboutListElementProps {
 
 export default function AboutListElement({title, list}: AboutListElementProps) {
     return (
-        <div className="flex justify-between itmes-center mb-2.5">
-            <span className="flex items-start text-[24px] font-medium pretendard text-whiteTransparent55 w-96 pr-1 h-8">
+        <div className="flex justify-between itmes-center w-[415px] mb-[10px]">
+            <span className="flex items-start text-[24px] font-medium pretendard text-whiteTransparent55">
                 {title}
             </span>
             <div className="flex-col items-center justify-start">
                 {list.map((name, index) => (
-                    <span key={index} className="flex itmes-start text-[24px] font-semibold pretendard text-white mb-1.5 h-8 w-40">{name}</span>
+                    <span key={index} className="flex itmes-start text-[24px] font-semibold pretendard text-white mb-[10px] h-[34px]">{name}</span>
                     ))
                 }
             </div>
