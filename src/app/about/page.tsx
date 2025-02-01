193eb0e7048ae676338aadb72cbc37f67d4c5e5b
이종원 <jongwon@ijong-won-ui-MacBookPro.local>
2025-02-02 00:03:05 +0900

    chore: 물결 제거

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,13 +5,16 @@ export default function AboutPage() {
   return (
     <div className="flex-col justify-start items-center bg-black">
       <h1 className="pretendard text-[36px] font-bold text-center p-[180px]">
-        ~웹 사이트를 만든 사람들~
+        웹 사이트를 만든 사람들
       </h1>
-      <div className="flex align-start justify-center overflow-y-hidden pb-[96px]"> 
+      <div className="flex align-start justify-center overflow-y-hidden pb-[96px]">
         {/* content */}
         <div className="w-[467px] h-[504px] mr-[88px]">
           <video autoPlay loop muted>
-              <source src="@/../../assets/videos/ending-credit.mp4" type="video/mp4" />
+            <source
+              src="@/../../assets/videos/ending-credit.mp4"
+              type="video/mp4"
+            />
           </video>
         </div>
         <div className="h-[504px]">
