fbc1f16970311122ad50a6629c04e9d91acf9c8e
mongsam2 <songjaehyun121476@gmail.com>
2025-02-01 19:01:19 +0900

    style: 헤더, 페이지 제목, 엔딩 크레딧 사이 간격 조정 (#7)

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,10 +4,10 @@ import dataList from "@/constants/about";
 export default function AboutPage() {
   return (
     <div className="flex-col justify-start items-center bg-black">
-      <h1 className="pretendard text-[36px] font-bold text-center p-12">
+      <h1 className="pretendard text-[36px] font-bold text-center p-[180px]">
         ~웹 사이트를 만든 사람들~
       </h1>
-      <div className="flex align-start justify-center overflow-y-hidden pt-5 pb-24"> 
+      <div className="flex align-start justify-center overflow-y-hidden pb-[96px]"> 
         {/* content */}
         <div className="w-96 h-96 mr-8">
           <video autoPlay loop muted>
