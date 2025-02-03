0b7a557b1a50926bedcf43f811f8d5d717a5d6be
ksundev <kshk08@hanyang.ac.kr>
2025-02-04 03:48:07 +0900

    feat: DB연결 DB 연결 (#44)

---
+++ src/components/interview/InterviewClient.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { useState } from "react";
+import InterviewTitle from "@/components/interview/title";
+import InterviewSelectGen from "@/components/interview/select_gen";
+import InterviewCard from "@/components/interview/card";
+
+type InterviewData = {
+  name: string;
+  generation: number;
+  role: string;
+  photoUrl: string;
+  description: string;
+};
+
+interface InterviewClientProps {
+  initialData: InterviewData[];
+}
+
+export default function InterviewClient({ initialData }: InterviewClientProps) {
+  const [gen, setGen] = useState<string>("13기");
+
+  const filteredData = initialData.filter(
+    (interview) => interview.generation === parseInt(gen.replace("기", ""))
+  );
+
+  return (
+    <div className="flex flex-col items-center w-full">
+      <div className="h-[130px]" />
+      <InterviewTitle />
+      <div className="h-[118px]" />
+      <div className="flex flex-col xl:w-[1040px]">
+        <InterviewSelectGen selectedGen={gen} setSelectedGen={setGen} />
+        <div className="h-[66px]" />
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 w-full">
+          {filteredData.map((interview, index) => (
+            <InterviewCard
+              key={index}
+              photoUrl={interview.photoUrl}
+              name={interview.name}
+              generation={interview.generation}
+              role={interview.role}
+              description={interview.description}
+            />
+          ))}
+        </div>
+        <div className="h-[200px]" />
+      </div>
+    </div>
+  );
+}
