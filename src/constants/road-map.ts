a097fdfe6caa2523cd6941322d03e53b9dc34e56
김예안 <kjan0929@naver.com>
2025-02-04 21:40:30 +0900

    chore: 로드맵 및 커리큘럼 데이터 업데이트 (#58)

--- src/constants/road-map.ts
+++ src/constants/road-map.ts
@@ -1,10 +1,10 @@
 const roadMap = [
-  { month: "2~3월", content: "아기사자 모집" },
-  { month: "2~3월", content: "아기사자 모집" },
-  { month: "2~3월", content: "아기사자 모집" },
-  { month: "2~3월", content: "아기사자 모집" },
-  { month: "2~3월", content: "아기사자 모집" },
-  { month: "2~3월", content: "아기사자 모집" },
+  { month: "2~3월", content: "아기사자 모집 및 OT" },
+  { month: "3~6월", content: "1학기 파트 별 세션" },
+  { month: "5월", content: "MT 및 중앙아이디어톤" },
+  { month: "7~8월", content: "중앙해커톤" },
+  { month: "9~12월", content: "2학기 트랙 별 세션" },
+  { month: "12월", content: "데모데이" },
 ];
 
 export { roadMap };
