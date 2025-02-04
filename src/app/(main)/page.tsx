f48eadbc29d86b6ec894955ae287b2eb151507f0
김예안 <kjan0929@naver.com>
2025-02-04 16:36:51 +0900

    feat: 메인 프로젝트 캐러셀 구현 (#51)

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,6 +3,7 @@ import LeadingCrew from "@/components/Main/leading-crew/LeadingCrew";
 import HeroBanner from "@/components/Main/hero/HeroBanner";
 import RoadMap from "@/components/Main/road-map/RoadMap";
 import TrackOverview from "@/components/Main/track-overview/TrackOverview";
+import ProjectPreview from "@/components/Main/project-preview/ProjectPreview";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <TrackOverview />
       <Curriculum />
       <LeadingCrew />
+      <ProjectPreview />
     </div>
   );
 }
---
+++ src/components/Main/project-preview/SlideCard.tsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+
+interface SlideCardProps {
+  generation: string;
+  crewMembers: string[];
+  imgUrl: string;
+}
+
+export default function SlideCard({
+  generation,
+  crewMembers,
+  imgUrl,
+}: SlideCardProps) {
+  return (
+    <div className="flex flex-col items-center gap-[21px] w-full h-[409px] mx-auto text-center">
+      <div>
+        <Image
+          src={imgUrl}
+          alt="carousel-image"
+          width={597}
+          height={364}
+          objectFit="cover"
+        />
+      </div>
+
+      <div className="flex items-center gap-[8px]">
+        <strong className="text-[20px] font-bold tracking-[0.005em] text-white">
+          프로젝트 명
+        </strong>
+
+        <div className="flex gap-[5px]">
+          <span className="text-[16px] font-medium tracking-[0.005em] text-white">
+            {generation}
+          </span>
+          <ul className="flex gap-[2px]">
+            {crewMembers.map((crew, index) => (
+              <li
+                className="text-[16px] font-medium tracking-[0.005em] text-white"
+                key={`crew-${index}`}
+              >
+                {crew}
+                {index < crewMembers.length - 1 && ","}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
