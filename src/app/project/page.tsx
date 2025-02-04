499195a2a937c4692d4413c9d0c10823e0daeae3
gn-ioeo <thdrkdms1030@hanyang.ac.kr>
2025-02-04 14:59:09 +0900

    feat: 페이지, 모달 API 연결

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,116 +1,59 @@
 "use client";
 
 import ArchivingSelectGen from "@/components/archiving/gen";
-import ProjectCards from "@/components/archiving/card"
-import { useState } from "react";
+import ProjectCards from "@/components/archiving/card";
+import { Suspense, useEffect, useState } from "react";
+
+interface Project {
+  id: number;
+  name: string;
+  description: string;
+  type: string;
+  generation: number;
+  thumbnailUrl: string;
+}
+
+async function getProjects() {
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_KEY}/projects`);
+    if (!response.ok) {
+      throw new Error(`${response.status}`);
+    }
+    const json = await response.json();
+    console.log(json);
+    return json;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
 
 export default function ProjectPage() {
   const [gen, setGen] = useState<string>("전체");
-  const projectData = [
-    {
-      id: 1,
-      title: "프로젝트 명",
-      type: "App",
-      description: "1프로젝트 소개 주루룩해서 주루룩해서 주루룩해서 두 줄까지도 가능 이렇게 저렇게 요롷게게",
-      images: [
-        "/project1-slide1.jpg",
-        "/project1-slide2.jpg",
-        "/project1-slide3.jpg",
-        "/project1-slide4.jpg"
-      ],
-      gen: 12
-    },
-    {
-      id: 2,
-      title: "프로젝트 명",
-      type: "App",
-      description: "2프로젝트 소개 주루룩해서 주루룩해서 주루룩해서 두 줄까지도 가능 이렇게 저렇게 요롷게게",
-      images: [
-        "/project1-slide1.jpg",
-        "/project1-slide2.jpg",
-        "/project1-slide3.jpg",
-        "/project1-slide4.jpg"
-      ],
-      gen: 12
-    },
-    {
-      id: 3,
-      title: "프로젝트 명",
-      type: "App",
-      description: "3프로젝트 소개 주루룩해서 주루룩해서 주루룩해서 두 줄까지도 가능 이렇게 저렇게 요롷게게",
-      images: [
-        "/project1-slide1.jpg",
-        "/project1-slide2.jpg",
-        "/project1-slide3.jpg",
-        "/project1-slide4.jpg"
-      ],
-      gen: 12
-    },
-    {
-      id: 4,
-      title: "프로젝트 명",
-      type: "App",
-      description: "4프로젝트 소개 주루룩해서 주루룩해서 주루룩해서 두 줄까지도 가능 이렇게 저렇게 요롷게게",
-      images: [
-        "/project1-slide1.jpg",
-        "/project1-slide2.jpg",
-        "/project1-slide3.jpg",
-        "/project1-slide4.jpg"
-      ],
-      gen: 13
-    },
-    {
-      id: 5,
-      title: "프로젝트 명",
-      type: "App",
-      description: "5프로젝트 소개 주루룩해서 주루룩해서 주루룩해서 두 줄까지도 가능 이렇게 저렇게 요롷게게",
-      images: [
-        "/project1-slide1.jpg",
-        "/project1-slide2.jpg",
-        "/project1-slide3.jpg",
-        "/project1-slide4.jpg"
-      ],
-      gen: 13
-    },
-    {
-      id: 6,
-      title: "프로젝트 명",
-      type: "App",
-      description: "6프로젝트 소개 주루룩해서 주루룩해서 주루룩해서 두 줄까지도 가능 이렇게 저렇게 요롷게게",
-      images: [
-        "/project1-slide1.jpg",
-        "/project1-slide2.jpg",
-        "/project1-slide3.jpg",
-        "/project1-slide4.jpg"
-      ],
-      gen: 13
-    },
-    {
-      id: 7,
-      title: "프로젝트 명",
-      type: "App",
-      description: "7프로젝트 소개 주루룩해서 주루룩해서 주루룩해서 두 줄까지도 가능 이렇게 저렇게 요롷게게",
-      images: [
-        "/project1-slide1.jpg",
-        "/project1-slide2.jpg",
-        "/project1-slide3.jpg",
-        "/project1-slide4.jpg"
-      ],
-      gen: 13
-    }
-   ];
+  const [projectData, setProjectData] = useState<Project[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getProjects();
+      setProjectData(data);
+    };
+    fetchData();
+  }, []);
 
   return (
     <div className="min-h-screen flex flex-col items-center">
       <div className="flex flex-col items-center gap-y-[12px] mt-[146px]">
-        <div className="py-[12px] px-[16px] bg-[#F08314] rounded-[4px] text-[24px] font-[700]">프로젝트 아카이빙</div>
+        <div className="py-[12px] px-[16px] bg-[#F08314] rounded-[4px] text-[24px] font-[700]">
+          프로젝트 아카이빙
+        </div>
         <div className="text-[36px] font-[700]">멋사 프로젝트 둘러보기</div>
       </div>
       <ArchivingSelectGen selectedGen={gen} setSelectedGen={setGen} />
-      <div className="flex-grow">
-        <ProjectCards projectData={projectData} selectedGen={gen} />
-      </div>
-      
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className="flex-grow">
+          <ProjectCards projectData={projectData} selectedGen={gen} />
+        </div>
+      </Suspense>
     </div>
   );
 }
--- src/components/archiving/modal.tsx
+++ src/components/archiving/modal.tsx
@@ -1,147 +1,195 @@
 "use client";
 
-interface Project { 
+interface SubProject {
   id: number;
-  images: string[];
-  title: string;
+  name: string;
   description: string;
   type: string;
-  gen: number;
+  createdAt: string;
+  deploymentUrl: string;
+  generation: number;
+  photos: { id: number; photoUrl: string }[];
+  projectMembers: { name: string; role: string }[];
 }
 
-interface ArchivingModalProps { 
-  project: Project;
+interface ArchivingModalProps {
+  sub: number;
   onClose: () => void;
 }
 
-import Close from '@/../public/assets/icons/close.svg'
-import Arrow from '@/../public/assets/icons/arrow_forward.svg'
-import Image from 'next/image'
-import {useState} from 'react';
+import Close from "@/../public/assets/icons/close.svg";
+import Arrow from "@/../public/assets/icons/arrow_forward.svg";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
+async function getProject(sub: number) {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_KEY}/projects/${sub}`
+    );
+    if (!response.ok) {
+      throw new Error(`${response.status}`);
+    }
+    const json = await response.json();
+    console.log(json);
+    return json;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
 
-export default function ArchivingModal({ project, onClose }: ArchivingModalProps) {
-
+export default function ArchivingModal({ sub, onClose }: ArchivingModalProps) {
   const [currentSlide, setCurrentSlide] = useState<number>(0);
+  const [project, setProject] = useState<SubProject | null>(null);
+  const photoCount = project?.photos.length || 0;
+  const roles: [string, string][] = [
+    ["Frontend", "프론트엔드"],
+    ["Backend", "백엔드"],
+    ["President", "기획/디자인"],
+  ];
 
-    const handlePrevSlide = () => {
-      const newSlide = currentSlide === 0 ? project.images.length - 1 : currentSlide - 1;
-      setCurrentSlide(newSlide);
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getProject(sub);
+      setProject(data);
     };
+    fetchData();
+  }, [sub]);
 
-    const handleNextSlide = () => {
-      const newSlide = currentSlide === project.images.length - 1 ? 0 : currentSlide + 1;
-      setCurrentSlide(newSlide);
-    };
+  const handlePrevSlide = () => {
+    const newSlide = currentSlide === 0 ? photoCount - 1 : currentSlide - 1;
+    setCurrentSlide(newSlide);
+  };
 
-    return (
-      <div onClick={onClose} className="z-50 flex justify-center items-center w-full h-full overflow-y-auto">
-        <div onClick={(e)=> e.stopPropagation()} className="flex flex-col p-[15px_18px] w-[738px] h-[914px] bg-background rounded-[14px] gap-[16px]">
-          <div className=" flex flex-row justify-between">
-            <div className="text-[18px] font-semibold h-[21px] text-white/75 ">Project</div>
-            <Image
-              src={Close}
-              alt="X"
-              width={24}
-              height={24}
-              className="opacity-75 cursor-pointer"
-              onClick={onClose}/>
+  const handleNextSlide = () => {
+    const newSlide = currentSlide === photoCount - 1 ? 0 : currentSlide + 1;
+    setCurrentSlide(newSlide);
+  };
+
+  return (
+    <div
+      onClick={onClose}
+      className="z-50 flex justify-center items-center w-full h-full overflow-y-auto"
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="flex flex-col p-[15px_18px] w-[738px] h-[914px] bg-background rounded-[14px] gap-[16px]"
+      >
+        <div className=" flex flex-row justify-between">
+          <div className="text-[18px] font-semibold h-[21px] text-white/75 ">
+            Project
           </div>
+          <Image
+            src={Close}
+            alt="X"
+            width={24}
+            height={24}
+            className="opacity-75 cursor-pointer"
+            onClick={onClose}
+          />
+        </div>
 
-          <div className="w-[701px] h-[394px] relative">
-
-            <Image 
-              src={project.images[currentSlide]}
+        <div className="w-[701px] h-[394px] relative">
+          {project && (
+            <Image
+              src={project.photos[currentSlide].photoUrl}
               alt={`프로젝트 이미지 ${currentSlide + 1}`}
               width={701}
               height={394}
-              className="h-[394px] shrink-0 rounded-[10px] object-cover bg-white"/>
-              
-              <button 
-                onClick={handlePrevSlide}
-                className={`absolute top-1/2 -translate-y-1/2 left-[18px] flex items-center justify-center p-[12px_16px] w-[44px] h-[44px] rounded-[84px] bg-black/20 hover:bg-black/30 ${
-                  currentSlide === 0 ? 'opacity-[0.5] cursor-not-allowed' : 'opacity-100'
-                }`}
-                disabled={currentSlide === 0}
-              >
-                <div className="w-[12px] h-[20px] rotate-180">
-                  <Image
-                    src={Arrow}
-                    alt="previous"
-                  />
-                </div>
-              </button>
-              <button 
-                onClick={handleNextSlide}
-                className={`absolute top-1/2 -translate-y-1/2 right-[18px] flex items-center justify-center p-[12px_16px] w-[44px] h-[44px] rounded-[84px] bg-black/20 hover:bg-black/30 ${
-                  currentSlide === project.images.length - 1 ? 'opacity-[0.5] cursor-not-allowed' : 'opacity-100'
-                }`}
-                disabled={currentSlide === project.images.length - 1}
-              >
-                <div className="w-[12px] h-[20px]">
-                  <Image
-                    src={Arrow}
-                    alt="next"
-                  />
-                </div>
-              </button>
-          </div>
-          <div className="flex flex-row gap-[26px]">
-            <div className="flex flex-col w-[195px] flex-start justify-center gap-[20px]">
-              <div className="inline-flex flex-col h-[256px] gap-[9px] p-[20px] bg-[#464242] rounded-[10px] ">
-                <div className="align-center gap-[40px] text-[18px] font-semibold h-[25px]">Team 3</div>
-                <div className="flex flex-col align-start w-[155px] gap-[22px]">
+              className="h-[394px] shrink-0 rounded-[10px] object-cover bg-white"
+            />
+          )}
 
-                  <div className="flex flex-col gap-[4px]">
-                    <div className="font-semibold text-white/55 text-[14px] h-[20px]">기획/디자인</div>
-                    <div className="text-[16px] font-semibold h-[22px] ">오승아</div>
-                  </div>
-                  <div className="flex flex-col gap-[4px]">
-                    <div className="font-semibold text-white/55 text-[14px] h-[20px]">프론트엔드</div>
-                    <div className="text-[16px] font-semibold h-[22px] ">이종원, 김선후, 김아영</div>
-                  </div>
-                  <div className="flex flex-col gap-[4px]">
-                    <div className="font-semibold text-white/55 text-[14px] h-[20px]">백엔드</div>
-                    <div className="text-[16px] font-semibold h-[22px] ">송재현, 이수민</div>
-                  </div>
-
-                </div>
+          <button
+            onClick={handlePrevSlide}
+            className={`absolute top-1/2 -translate-y-1/2 left-[18px] flex items-center justify-center p-[12px_16px] w-[44px] h-[44px] rounded-[84px] bg-black/20 hover:bg-black/30 ${
+              currentSlide === 0
+                ? "opacity-[0.5] cursor-not-allowed"
+                : "opacity-100"
+            }`}
+            disabled={currentSlide === 0}
+          >
+            <div className="w-[12px] h-[20px] rotate-180">
+              <Image src={Arrow} alt="previous" />
+            </div>
+          </button>
+          <button
+            onClick={handleNextSlide}
+            className={`absolute top-1/2 -translate-y-1/2 right-[18px] flex items-center justify-center p-[12px_16px] w-[44px] h-[44px] rounded-[84px] bg-black/20 hover:bg-black/30 ${
+              currentSlide === photoCount - 1
+                ? "opacity-[0.5] cursor-not-allowed"
+                : "opacity-100"
+            }`}
+            disabled={currentSlide === photoCount - 1}
+          >
+            <div className="w-[12px] h-[20px]">
+              <Image src={Arrow} alt="next" />
+            </div>
+          </button>
+        </div>
+        <div className="flex flex-row gap-[26px]">
+          <div className="flex flex-col w-[195px] flex-start justify-center gap-[20px]">
+            <div className="inline-flex flex-col h-[256px] gap-[9px] p-[20px] bg-[#464242] rounded-[10px] ">
+              <div className="align-center gap-[40px] text-[18px] font-semibold h-[25px]">
+                Team 3
               </div>
-
-              <div className="p-[20px] bg-[#464242] rounded-[10px] ">               
-                <div className="flex flex-col align-start w-[155px] gap-[22px]">
-                  <div className="flex flex-col gap-[4px]">
-                    <div className="font-semibold text-white/55 text-[14px] h-[20px]">프로젝트 기간</div>
-                    <div className="text-[16px] font-semibold h-[22px] ">2024. 07~2024. 08</div>
+              <div className="flex flex-col align-start w-[155px] gap-[22px]">
+                {roles.map(([english, korean]) => (
+                  <div key={english} className="flex flex-col gap-[4px]">
+                    <div className="font-semibold text-white/55 text-[14px] h-[20px]">
+                      {korean}
+                    </div>
+                    {project?.projectMembers
+                      .filter((member) => member.role === english)
+                      .map((member, index) => (
+                        <div
+                          key={index}
+                          className="text-[16px] font-semibold h-[22px]"
+                        >
+                          {" "}
+                          {member.name}{" "}
+                        </div>
+                      ))}
                   </div>
-                  <div className="flex flex-col gap-[4px]">
-                    <div className="font-semibold text-white/55 text-[14px] h-[20px]">프로젝트 링크</div>
-                    <div className="text-[16px] font-semibold h-[22px] ">-</div>
+                ))}
+              </div>
+            </div>
+
+            <div className="p-[20px] bg-[#464242] rounded-[10px] ">
+              <div className="flex flex-col align-start w-[155px] gap-[22px]">
+                <div className="flex flex-col gap-[4px]">
+                  <div className="font-semibold text-white/55 text-[14px] h-[20px]">
+                    프로젝트 기간
+                  </div>
+                  <div className="text-[16px] font-semibold h-[22px] ">
+                    {project?.createdAt}
+                  </div>
+                </div>
+                <div className="flex flex-col gap-[4px]">
+                  <div className="font-semibold text-white/55 text-[14px] h-[20px]">
+                    프로젝트 링크
+                  </div>
+                  <div className="text-[16px] font-semibold h-[22px] ">
+                    {project?.deploymentUrl}
                   </div>
                 </div>
               </div>
             </div>
-            <div className="flex flex-col w-[481px] gap-[7px]">
-              <div className="inline-flex items-center gap-[12px]">
-                <div className="text-[24px] font-semibold">{project.title}</div>
-                <div className="text-[18px] font-semibold text-white/40">{project.type}</div>
+          </div>
+          <div className="flex flex-col w-[481px] gap-[7px]">
+            <div className="inline-flex items-center gap-[12px]">
+              <div className="text-[24px] font-semibold">{project?.name}</div>
+              <div className="text-[18px] font-semibold text-white/40">
+                {project?.type}
               </div>
-              <div className="text-[18px] text-white/75">
-                Z세대, 웰니스 중에서도 정신건강에 관심이 높아졌는데요, 하지만, Z세대의 정신건강 어떻게 채워야 할까요?
-
-                포레스트는 힐링 이미지를 통한 테라피와 Z세대의 성향에 맞춘 유쾌한 웹사이트 Therapy입니다.
-
-                Therapy - 미술관에 들어온 듯, 갤러리 테라피
-                Creation - 크리에이터이자 유저가 이미지를 생산 후에 굿즈로 재탄생
-                Joy - MZ 타겟을 위한 이미지 콘테스트
-
-
-                당신의 편안한
-                쉼을 위해, 포레스트.
-              </div>
+            </div>
+            <div className="text-[18px] text-white/75">
+              {project?.description}
             </div>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
