5849e6e839c88ffc7f41e39ee1aa09ac4084e505
이종원 <jongwon@ijong-won-ui-MacBookPro.local>
2025-02-07 13:25:50 +0900

    chore: video -> GIF 파일 변경

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["storage.googleapis.com"],
+    domains: ["storage.googleapis.com", "s3-alpha-sig.figma.com"],
   },
 };
 
