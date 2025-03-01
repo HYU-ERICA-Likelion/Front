3e2466349399f32f471ddab151365398a9dec3da
이종원 <jongwon@ijong-won-ui-MacBookPro.local>
2025-03-01 22:37:31 +0900

    chore: 운영진 사진 수정

--- src/components/interview/profile.tsx
+++ src/components/interview/profile.tsx
@@ -14,7 +14,7 @@ export default function Profile({ img, role, name }: Props) {
         width={156}
         height={156}
         alt="profile"
-        className="w-[156px] h-[156px]"
+        className="w-[156px] h-[156px] rounded-full"
       />
       <div className="h-[20px]" />
       <div className="text-[24px] font-semibold text-center">{role}</div>
