d7c913d27cb459a99f20fe5c6842372ccc035eeb
김예안 <kjan0929@naver.com>
2025-03-26 20:02:22 +0900

    feat: 매칭 완료 모달 UI 구현 (#111)

--- src/app/team-maker/matching-result/page.tsx
+++ src/app/team-maker/matching-result/page.tsx
@@ -1,16 +1,24 @@
 "use client";
 
-import AdminAuthModal from "@/components/TeamMaker/Modal/AdminAuthModal";
+import MatchiingSuccessModal from "@/components/TeamMaker/Modal/MatchingSuccessModal";
 import useToggle from "@/hooks/useToggle";
+import mockData from "@/mocks/teamDTOList.json";
 
 export default function MatchingResultPage() {
   const { isOpen, toggle } = useToggle();
+
+  const { teamDtoList } = mockData;
 
   return (
     <div>
       <button onClick={toggle}>Trigger button</button>
 
-      <AdminAuthModal isOpen={isOpen} toggle={toggle} />
+      {/* <AdminAuthModal isOpen={isOpen} toggle={toggle} /> */}
+      <MatchiingSuccessModal
+        teamData={teamDtoList}
+        isOpen={isOpen}
+        toggle={toggle}
+      />
     </div>
   );
 }
