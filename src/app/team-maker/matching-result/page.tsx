553c112e4d080f53f8067265c7919782b47ea441
김예안 <kjan0929@naver.com>
2025-03-26 13:48:33 +0900

    style: 어드민 인증 모달 UI 구현 (#111)

--- src/app/team-maker/matching-result/page.tsx
+++ src/app/team-maker/matching-result/page.tsx
@@ -1,3 +1,16 @@
+"use client";
+
+import AdminAuthModal from "@/components/TeamMaker/Modal/AdminAuthModal";
+import useToggle from "@/hooks/useToggle";
+
 export default function MatchingResultPage() {
-  return <div>MatchingResultPage</div>;
+  const { isOpen, toggle } = useToggle();
+
+  return (
+    <div>
+      <button onClick={toggle}>Trigger button</button>
+
+      <AdminAuthModal isOpen={isOpen} toggle={toggle} />
+    </div>
+  );
 }
